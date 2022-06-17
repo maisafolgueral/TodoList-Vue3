1ce8e5c1b3026f927883885195cce760107c3149
Maisa <seu e-mail>
2022-06-17 16:38:42 -0300

    TodoList

---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+  <input type="text" @change="addToList" v-model="text">
+  <ul>
+    <li v-for="(item, index) in list" :key="index">
+      {{item}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+      text: ''
+    }
+  },
+  methods: {
+    addtoList() {
+      this.list.unshift(this.text);
+      this.text = '';
+    }
+  }
+}
+</script>
+
+<style>
+</style>
