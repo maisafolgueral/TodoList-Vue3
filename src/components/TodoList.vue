2b84c5a6e5034dd57659c31ce3d9961c00a2996d
Maisa <seu e-mail>
2022-06-17 17:14:27 -0300

    Styling

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,17 @@
 <template>
-  <input type="text" @change="addToList" v-model="text">
-  <ul>
-    <li v-for="(item, index) in list" :key="index">
-     <span> {{item}} </span>
-     <span @click="deleteFromList(index)"> || delete</span>
-    </li>
-  </ul>
+<div class="todo-list">
+    <h1>Todo-List</h1>
+    <input class="text-field" type="text" @change="addToList" v-model="text" />
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        <span @click="toggleCheckbox(item)"> 
+          <input type="checkbox" :checked="item.done">
+          <span :class="{'done' : item.done}">{{item.label}}</span>
+        </span>
+        <span @click="deleteFromList(index)"> || delete</span>
+      </li>
+    </ul> 
+</div>
 </template>
 
 <script>
@@ -13,29 +19,61 @@ export default {
   data() {
     return {
       list: [],
-      text: ''
-    }
+      text: "",
+    };
   },
   created() {
-    this.list = JSON.parse(localStorage.getItem('list')) || [];
+    this.list = JSON.parse(localStorage.getItem("list")) || [];
   },
   methods: {
     addtoList() {
-      this.list.unshift(this.text);
+      this.list.unshift({label: this.text, done: false});
       this.updateLocalStorage();
-      this.text = '';
+      this.text = "";
     },
     deleteFromList(index) {
       this.list.splice(index, 1);
       this.updateLocalStorage();
     },
     updateLocalStorage() {
-      localStorage.setItem('list', JSON.stringify(this.list));
+      localStorage.setItem("list", JSON.stringify(this.list));
+    },
+    toggleCheckbox(item) {
+      item.done = !item.done;
+      this.updateLocalStorage();
     }
-  }
-}
+  },
+};
 </script>
 
 <style>
+  .todo-list {
+    max-width: 500px;
+    margin: auto;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  .text-field {
+    width: 100%;
+    height: 35px;
+    margin-bottom: 15px;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .done {
+    text-decoration: line-through;
+  }
 
 </style>
